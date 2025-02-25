6960e27c96f9372e525a20151684308b32d50b46
Tom Dai <djttom@yahoo.com>
2025-02-24 21:06:59 -0800

    update logic operation

--- code/logic.c
+++ code/logic.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int main(void)
+int main (void)
 {
     int x = 13;
+    int y = 0;
 
     printf("before: x is %d\n", x);
     x = x == x;
@@ -12,14 +13,20 @@ int main(void)
     printf("\n");
     printf("true is %d, false is %d\n", true, false);
 
+    printf("\n");
+    printf("y is 0: %s\n", y == 0 ? "true" : "false");
+
     return 0;
 }
 
+// below is the output
 #if 0
 
 before: x is 13
 after: x is 1
 
 true is 1, false is 0
+
+y is 0: true
 
 #endif
